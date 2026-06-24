fc608e80dac37e1f2a76671cbda7cd9582918220
Merlyn Francis <merlyn.francis@gmail.com>
2026-06-24 11:58:46 -0700

    initial commit

---
+++ src/main/java/guru/springframework/springDIwebapp/SpringDIwebappApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.springDIwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDIwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDIwebappApplication.class, args);
+	}
+
+}
