021c9f37b799b66b63a472aa9eaee26976990a4d
Merlyn Francis <merlyn.francis@gmail.com>
2026-06-24 12:21:43 -0700

    controller with Spring ctx

--- src/main/java/guru/springframework/springDIwebapp/SpringDIwebappApplication.java
+++ src/main/java/guru/springframework/springDIwebapp/SpringDIwebappApplication.java
@@ -1,13 +1,20 @@
 package guru.springframework.springDIwebapp;
 
+import guru.springframework.springDIwebapp.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringDIwebappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDIwebappApplication.class, args);
+
+		ApplicationContext ctx = SpringApplication.run(SpringDIwebappApplication.class, args);
+
+		MyController controller = ctx.getBean(MyController.class);
+		System.out.println("In the Main method");
+		System.out.println(controller.sayHello());
 	}
 
 }
